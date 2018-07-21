ba09f4d83d9d7154b569f4b6c4e6d15363af109a
PVThomas <ptv801@gmail.com>
2018-07-21 10:23:13 +0530

    Heroku ready.

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ var express = require('express');
 var hbs = require('hbs');
 hbs.registerPartials(__dirname+'/views/partials')
 var app = express();
+const port = process.env.PORT || 3000;
+app.use((req,res,next)=>{
+res.send('Under Maintanance.');
+});
 app.get('/',(req,res)=>{
     res.render('./index.hbs',{currentYear: new Date().getFullYear()});
 })
+
 app.use(express.static(__dirname+'/public'));
 app.set('view engine','hbs');
 app.listen(3000,()=>{
-    console.log('Server running on port: 3000');
+    console.log(`Server running on port: ${port}`);
 });
